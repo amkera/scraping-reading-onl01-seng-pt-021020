38f1191118e6a6b89ab7a1eabcefebccb326b1ad
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-03-16 04:37:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+ 
+doc = Nokogiri::HTML(open("https://flatironschool.com/")).css(".headline-260IBN").text 
 
-html = open("https://flatironschool.com/")
 
-doc = Nokogiri::HTML(html)
